bb3ccbcccae1d55f67190a674d0bf63c7d8edd06
Lucky Barkane <Lucky.Barkane@incture.com>
2018-03-09 23:43:08 +0530

    version 1

---
+++ src/main/java/csv/CsvToJson.java
@@ -0,0 +1,34 @@
+package csv;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+
+public class CsvToJson {
+
+	public static void main(String[] args) throws JsonProcessingException, IOException {
+		File input = new File("attachment.csv");
+		File output = new File("output.json");
+
+		CsvSchema csvSchema = CsvSchema.builder().setUseHeader(true).build();
+		CsvMapper csvMapper = new CsvMapper();
+
+		// Read data from CSV file
+		List<Object> readAll = csvMapper.readerFor(Map.class).with(csvSchema).readValues(input).readAll();
+
+		ObjectMapper mapper = new ObjectMapper();
+
+		// Write JSON formated data to output.json file
+		mapper.writerWithDefaultPrettyPrinter().writeValue(output, readAll);
+
+		// Write JSON formated data to stdout
+		System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(readAll));
+	}
+
+}
